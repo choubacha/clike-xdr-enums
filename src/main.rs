c61e4c8d6022282c8c672f407e3ebec3f6f2d3e1
Kevin Choubacha <chewbacha@gmail.com>
2018-05-13 11:48:23 -0700

    Serializing a struct

--- src/main.rs
+++ src/main.rs
@@ -8,10 +8,19 @@ enum Clike {
     Two = 2,
 }
 
+#[derive(Serialize, Deserialize, Debug)]
+struct Foo {
+    bar: Clike,
+}
+
 fn main() {
     let v = serde_xdr::to_bytes(&Clike::One);
     println!("{:?}", v);
 
     let v = serde_xdr::to_bytes(&Clike::Two);
+    println!("{:?}", v);
+
+    let s = Foo { bar: Clike::One };
+    let v = serde_xdr::to_bytes(&s);
     println!("{:?}", v);
 }
